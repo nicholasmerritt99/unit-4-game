a2a72c797716a7a66bb679c51897af3c6b2154dc
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-03-24 14:30:03 -0500

    added to my Java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,19 @@
 $( document ).ready(function(){
-    var Random= Math.floor(Math.random()*100+19)
-    // Selects a random number to be shown at the start of the game
-    // Number should be should be between 19 - 120
-    //
-    $('#randomNumber').text(Random);
-    // Appending random number to the randomNumber id in the html doc
-    //
-    var num1= Math.floor(Math.random()*15+1)
-    var num2= Math.floor(Math.random()*15+1)
-    var num3= Math.floor(Math.random()*15+1)
-    var num4= Math.floor(Math.random()*15+1)
-    // Setting up random numbers for each jewel
-    // Random number has to be between 1 - 12
-    // 
+
+
+    var Random= Math.floor(Math.random()* 99 + 19)
+    var num1= Math.floor(Math.random()* 15 + 1)
+    var num2= Math.floor(Math.random()* 15 + 1)
+    var num3= Math.floor(Math.random()* 15 + 1)
+    var num4= Math.floor(Math.random()* 15 + 1)
     var userTotal= 0; 
     var wins= 0;
     var losses = 0;
-    //  Decaring variables for tallies
+   
+
   $('#numberWins').text(wins);
   $('#numberLosses').text(losses);
-
+  $('#randomNumber').text(Random);
 
   function winner(){
   alert("No way! You won!");
@@ -27,18 +21,17 @@ $( document ).ready(function(){
     $('#numberWins').text(wins);
     reset();
   }
-  //addes the losses to the userTotal
+
   function loser(){
   alert ("I will always win! You lose!");
     losses++;
     $('#numberLosses').text(losses);
     reset()
   }
-  //sets up click for jewels
+
     $('#crystalOne').on ('click', function(){
       userTotal = userTotal + num1;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
+      $('#finalScore').text(userTotal); 
             //sets win/lose conditions
           if (userTotal == Random){
             winner();
@@ -49,8 +42,7 @@ $( document ).ready(function(){
     })  
     $('#crystalTwo').on ('click', function(){
       userTotal = userTotal + num2;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
+      $('#finalScore').text(userTotal); 
           if (userTotal == Random){
             winer();
           }
@@ -60,9 +52,8 @@ $( document ).ready(function(){
     })  
     $('#crystalThree').on ('click', function(){
       userTotal = userTotal + num3;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal);
-  //sets win/lose conditions
+      $('#finalScore').text(userTotal);
+
             if (userTotal == Random){
                 winner();
           }
@@ -72,8 +63,7 @@ $( document ).ready(function(){
     })  
     $('#crystalFour').on ('click', function(){
       userTotal = userTotal + num4;
-      console.log("New userTotal= " + userTotal);
-      $('#finalTotal').text(userTotal); 
+      $('#finalScore').text(userTotal); 
         
             if (userTotal == Random){
                 winner();
@@ -85,13 +75,12 @@ $( document ).ready(function(){
 
     function reset(){
         Random=Math.floor(Math.random()*101+19);
-        console.log(Random)
         $('#randomNumber').text(Random);
         num1= Math.floor(Math.random()*11+1);
         num2= Math.floor(Math.random()*11+1);
         num3= Math.floor(Math.random()*11+1);
         num4= Math.floor(Math.random()*11+1);
         userTotal= 0;
-        $('#finalTotal').text(userTotal);
+        $('#finalScore').text(userTotal);
         } 
   }); 
